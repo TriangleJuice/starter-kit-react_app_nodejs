46829a3029403bd7912d3ce675062df1f8ce8cbd
Jasper Van Proeyen <trianglejuice@mac.com>
2018-12-17 11:38:17 +0100

    Made branding mapping more readable

--- utils/branding.js
+++ utils/branding.js
@@ -1,8 +1,23 @@
 function mapBranding(val) {
 	switch (val) {
-		case 'ACPaaS': return {cdn: 'acpaas_branding_scss', npm: ['@a-ui/core', '@a-ui/acpaas'], version: '3.0.3', type: 'acpaas' };
-		case 'Digipolis': return {cdn: 'digipolis_branding_scss', npm: ['@a-ui/core', '@a-ui/digipolis'], version: '3.0.2', type: 'digipolis' };
-		default: return {cdn: 'core_branding_scss', npm: ['@a-ui/core'], version: '3.0.3', type: 'core' };
+		case 'ACPaaS': return {
+				cdn: 'acpaas_branding_scss',
+				npm: ['@a-ui/core', '@a-ui/acpaas'],
+				version: '3.0.3',
+				type: 'acpaas',
+			};
+		case 'Digipolis': return {
+				cdn: 'digipolis_branding_scss',
+				npm: ['@a-ui/core', '@a-ui/digipolis'],
+				version: '3.0.2',
+				type: 'digipolis',
+			};
+		default: return {
+				cdn: 'core_branding_scss',
+				npm: ['@a-ui/core'],
+				version: '3.0.3',
+				type: 'core',
+			};
 	}
 }
 
