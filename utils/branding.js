4543153edcb365e8ff093332272bf9a8e323a5f1
Jasper Van Proeyen <trianglejuice@mac.com>
2018-12-17 11:45:29 +0100

    Even better indenting

--- utils/branding.js
+++ utils/branding.js
@@ -1,18 +1,21 @@
 function mapBranding(val) {
 	switch (val) {
-		case 'ACPaaS': return {
+		case 'ACPaaS':
+			return {
 				cdn: 'acpaas_branding_scss',
 				npm: ['@a-ui/core', '@a-ui/acpaas'],
 				version: '3.0.3',
 				type: 'acpaas',
 			};
-		case 'Digipolis': return {
+		case 'Digipolis':
+			return {
 				cdn: 'digipolis_branding_scss',
 				npm: ['@a-ui/core', '@a-ui/digipolis'],
 				version: '3.0.2',
 				type: 'digipolis',
 			};
-		default: return {
+		default:
+			return {
 				cdn: 'core_branding_scss',
 				npm: ['@a-ui/core'],
 				version: '3.0.3',
