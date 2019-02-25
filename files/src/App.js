923f6aac47ddf1144f7e1382ec12a194d34b94a0
Olivier Van den Mooter <oliviervandenmooter@Oliviers-MacBook-Pro.local>
2019-02-25 16:21:13 +0100

    typo

--- files/src/App.js
+++ files/src/App.js
@@ -5,7 +5,6 @@ import {
   Footer,
 } from '@acpaas-ui/react-components';
 /**
-    await start(config);
  * More ACPaaS UI documentation can be found here:
  * https://digipolisantwerp.github.io/acpaas-ui_react/
  */
