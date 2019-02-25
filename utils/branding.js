105ff74a2610b8c7d2f8d1728c035cb8eba8e610
Olivier Van den Mooter <oliviervandenmooter@Oliviers-MacBook-Pro.local>
2019-02-25 17:17:36 +0100

    Get latest version of branding
     - Get latest version of the branding grom npmjs & apply to link

--- utils/branding.js
+++ utils/branding.js
@@ -1,8 +1,10 @@
+const package = require('./package');
+
 const brandings = {
   ACPaaS: {
     cdn: 'acpaas_branding_scss',
     npm: ['@a-ui/core', '@a-ui/acpaas'],
-    version: '3.0.3',
+    version: 0,
     type: 'acpaas',
     key: 'ACPaaS',
   },
@@ -22,12 +24,25 @@ const brandings = {
   },
 };
 
-function mapBranding(key) {
-  if (key in brandings) return brandings[key];
-  return brandings.Antwerp;
+const getBrandings = async () => {
+  const keys = Object.keys(brandings);
+  const updatedBrandings = {};
+  await Promise.all(keys.map(async (key) => {
+    const branding = brandings[key];
+    branding.version = await package.getlatestverion(branding.npm);
+    updatedBrandings[key] = branding;
+    return branding;
+  }));
+  return updatedBrandings;
+}
+
+async function mapBranding(key) {
+  if (key in brandings) return (await getBrandings())[key];
+  return (await getBrandings()).Antwerp;
 }
 
 module.exports = {
   mapBranding,
+  getBrandings,
   brandings,
 };
