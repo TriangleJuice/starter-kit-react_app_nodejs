a0e5d9b863f6a94de661d511712aef95a371d5d0
Tom Ceuppens <t-c@live.be>
2019-09-03 09:26:11 +0200

    rename to scss

--- generators/angular/files/extra/src/app/pages/home/home.page.ts
+++ generators/angular/files/extra/src/app/pages/home/home.page.ts
@@ -4,7 +4,7 @@ import {
 
 @Component({
   templateUrl: './home.page.html',
-  styles: ['./home.page.css']
+  styles: ['./home.page.scss']
 })
 export class HomePage {
   // placeholder for rendering pages with headers and footers
